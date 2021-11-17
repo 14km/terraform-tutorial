cbeb2a4db8473ee29b81e7dde164555aeab26346
Connor Kim <k.connor614@gmail.com>
2021-11-17 23:49:32 +0900

    ec2 tf 변경해보기

--- modules/api-gateway-service/ec2.tf
+++ modules/api-gateway-service/ec2.tf
@@ -1,8 +1,25 @@
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = data.aws_ami.ubuntu
   instance_type = "t2.micro"
 
   tags = {
     Name = "AppServerInstance"
   }
+}
+
+data "aws_ami" "ubuntu" {
+  // 가장 최근의 값
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
